d702d93ab292cc0e27eddb8429fceedb2eb4f278
Prashant Goel <prashantmanunited@gmail.com>
2021-09-02 15:47:20 +0530

    ci: add lighthouse along with circleci

---
+++ .lighthouserc.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+const port = process.env.LIGHTHOUSE_CI_PORT;
+
+module.exports = {
+  ci: {
+    collect: {
+      chromePath: puppeteer.executablePath(),
+      settings: {
+        chromeFlags: [
+          '--headless',
+          'CI' in process.env && '--no-sandbox',
+        ].filter(Boolean),
+      },
+      numberOfRuns: 1,
+      startServerCommand: 'yarn start',
+      startServerReadyPattern: 'ready - started server',
+      startServerReadyTimeout: 10000,
+      url: [`http://localhost:${port}`],
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 0.95 }],
+      },
+    },
+  },
+};
